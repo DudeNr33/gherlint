7cf02f995b34a2cc382e3d6cecb2ba3a19d48fb9
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2021-10-12 17:20:49 +0200

    Add ``missing-scenario-name``

--- src/gherlint/checkers/completeness.py
+++ src/gherlint/checkers/completeness.py
@@ -3,16 +3,32 @@ Checker focussing of completeness of feature files, e. g. that every scenario mu
 every parameter that is used in a scenario outline has to be defined in the examples, etc.
 """
 
+from typing import Union
+
 from gherlint.checkers.base_checker import BaseChecker
-from gherlint.objectmodel.nodes import Feature
+from gherlint.objectmodel import nodes
 from gherlint.reporting import Message
 
 
 class CompletenessChecker(BaseChecker):
     """Concerned about things like missing examples etc."""
 
-    def visit_feature(self, node: Feature) -> None:
+    def visit_feature(self, node: nodes.Feature) -> None:
         if not node.name.strip():
             self.reporter.add_message(
                 Message("missing-feature-name", "Feature has no name"), node
+            )
+
+    def visit_scenario(self, node: nodes.Scenario) -> None:
+        self._check_missing_scenario_name(node)
+
+    def visit_scenariooutline(self, node: nodes.ScenarioOutline) -> None:
+        self._check_missing_scenario_name(node)
+
+    def _check_missing_scenario_name(
+        self, node: Union[nodes.Scenario, nodes.ScenarioOutline]
+    ) -> None:
+        if not node.name.strip():
+            self.reporter.add_message(
+                Message("missing-scenario-name", "Scenario has no name"), node
             )
---
+++ tests/functional/m/missing_scenario_name.feature
@@ -0,0 +1,11 @@
+Feature: Test
+
+    Scenario:
+        Given some precondition
+        When I do something
+        Then I expect some outcome
+
+    Scenario Outline:
+        Given some precondition
+        When I do something
+        Then I expect some outcome
